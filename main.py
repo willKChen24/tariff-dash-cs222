10d5c934456a0e19b9f80aa38249a3438ab53c7c
Will Chen <wchen226@illinois.edu>
2026-04-08 18:52:09 -0500

    feat: basic UI and functionality

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import streamlit as st
 import pandas as pd
 import requests
 
+#globals
+country_codes = { #country codes (values) must be lowercase for API endpoint
+    "US" : "usa",
+    "China" : "chn",
+    "European Union" : "deu",
+    "Japan" : "jpn",
+    "Canada" : "can"
+}
+
 #basic dashboard functions
 def atr_and_top_3_tr(json_response):
     tariff_series = json_response['dataSets'][0]['series']
@@ -36,23 +45,26 @@ def atr_and_top_3_tr(json_response):
 
     return atr, top_3_tr_categories
 
+#UI contents
+
 st.title("TariffDash- A Tariff Analysis Dashboard")
-select_country = st.selectbox("Select a country's tariff to analyze", ["USA", "Japan", "Mexico"])
+select_country = st.selectbox("Select a country's tariff to analyze", ["US", "China", "European Union", "Japan", "Canada"])
+country_code = country_codes[select_country]
 st.write(f"You selected {select_country} for analysis.")
 select_year = st.selectbox("Select a year for tariff data", ["2020", "2021", "2022"])
 
-if select_country == "USA":
-    #change this URL to the actual API endpoint for USA tariff data
-    response = requests.get("https://wits.worldbank.org/API/V1/SDMX/V21/datasource/tradestats-tariff/reporter/usa/year/2022/partner/WLD/product/all/indicator/AHS-SMPL-AVRG?format=JSON")
-    response_body = response.json()
-    st.write("USA Tariff Data:")
+response = requests.get(f"https://wits.worldbank.org/API/V1/SDMX/V21/datasource/tradestats-tariff/reporter/{country_code}/year/{select_year}/partner/WLD/product/all/indicator/AHS-SMPL-AVRG?format=JSON")
+print(f"Status: {response.status_code}")
+response_body = response.json()
+print(response)
+st.write("USA Tariff Data:")
 
-    tariff_stats = atr_and_top_3_tr(response_body)
-    st.metric(label=f"Average Tariff Rate for {select_country}", value=f"{tariff_stats[0]}%")
-    top_3_tr_categories = tariff_stats[1]
-    top_3_tr_df = pd.DataFrame(top_3_tr_categories)
-    st.write("Top 3 Tariff Rates and Categories:")
-    st.dataframe(top_3_tr_df)
+tariff_stats = atr_and_top_3_tr(response_body)
+st.metric(label=f"Average Tariff Rate for {select_country}", value=f"{tariff_stats[0]}%")
+top_3_tr_categories = tariff_stats[1]
+top_3_tr_df = pd.DataFrame(top_3_tr_categories)
+st.write("Top 3 Tariff Rates and Categories:")
+st.dataframe(top_3_tr_df)
 
 # if __name__ == "__main__":
 
